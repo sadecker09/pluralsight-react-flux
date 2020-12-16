92a6c38ba3779a7714a3ddffb34dec48e2737395
Sara Decker <sdecker@WEST-INC.LOCAL>
2020-12-15 17:59:22 -0700

    add centralized dispatcher, and set up first action; created actionTypes constant file.

---
+++ src/actions/courseActions.js
@@ -0,0 +1,18 @@
+import dispatcher from "../appDispatcher";
+import * as courseApi from "../api/courseApi";
+import actionTypes from "./actionTypes";
+
+// this function is the action creator
+export function saveCourse(course) {
+  // return the promise that is generated right here from calling courseApi.saveCourse
+  // this way the caller will be notified when the promise resolves
+  return courseApi.saveCourse(course).then((savedCourse) => {
+    // this registers the action w/ the dispatcher
+    // ("go tell all the stores that a course was just updated")
+    dispatcher.dispatch({
+      // this is the action (payload)
+      actionType: actionTypes.CREATE_COURSE,
+      course: savedCourse,
+    });
+  });
+}
